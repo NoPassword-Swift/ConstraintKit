d9f49eee10ea34d6fa5228f59f9d21c82b10af3b
David Skrundz <david@skrundz.ca>
2022-07-25 21:41:14 -0600

    Fill with system spacing

--- Sources/ConstraintKit/Fill.swift
+++ Sources/ConstraintKit/Fill.swift
@@ -35,4 +35,32 @@ extension View {
 		precondition(self.superview != nil, "Cannot fill superview if it is nil")
 		return self.fill(view: self.superview!, inset: inset)
 	}
+
+	/// Sets `self` to be equal to `view` with insets
+	@available(macOS 11.0, *)
+	public func fillSystemSpacing(view: View, multipliers: EdgeInsets) -> [NSLayoutConstraint] {
+		[
+			self.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: multipliers.top),
+			self.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: multipliers.left),
+			view.trailingAnchor.constraint(equalToSystemSpacingAfter: self.trailingAnchor, multiplier: multipliers.right),
+			view.bottomAnchor.constraint(equalToSystemSpacingBelow: self.bottomAnchor, multiplier: multipliers.bottom),
+		]
+	}
+
+	@available(macOS 11.0, *)
+	public func fillSystemSpacing(view: View, multiplier: CGFloat = 0) -> [NSLayoutConstraint] {
+		self.fillSystemSpacing(view: view, multipliers: EdgeInsets(top: multiplier, left: multiplier, bottom: multiplier, right: multiplier))
+	}
+
+	@available(macOS 11.0, *)
+	public func fillSuperviewSystemSpacing(multipliers: EdgeInsets) -> [NSLayoutConstraint] {
+		precondition(self.superview != nil, "Cannot fill superview if it is nil")
+		return self.fillSystemSpacing(view: self.superview!, multipliers: multipliers)
+	}
+
+	@available(macOS 11.0, *)
+	public func fillSuperviewSystemSpacing(multiplier: CGFloat = 0) -> [NSLayoutConstraint] {
+		precondition(self.superview != nil, "Cannot fill superview if it is nil")
+		return self.fillSystemSpacing(view: self.superview!, multiplier: multiplier)
+	}
 }
